3abfa4e1aa958b79f607086da8b259b0d55822fa
Mario Velasco Alonso <mario1velasco@gmail.com>
2024-12-27 17:46:11 +0100

    Feat: COmment valies

--- apps/my-personal-project/src/environments/environment.prod.ts
+++ apps/my-personal-project/src/environments/environment.prod.ts
@@ -3,11 +3,11 @@ import { EnvironmentModel } from './environment.model';
 export const environment: EnvironmentModel = {
   apiUrl: 'prod',
   production: true,
-  apiKey: 'AIzaSyBiAJYmhWQxqdSNEpDcXANNWHNaDdLQtMc',
-  authDomain: 'my-personal-project-7c70d.firebaseapp.com',
-  projectId: 'my-personal-project-7c70d',
-  storageBucket: 'my-personal-project-7c70d.firebasestorage.app',
-  messagingSenderId: '938956948971',
-  appId: '1:938956948971:web:34ade1fc4fb53a773a45ae',
-  measurementId: 'G-422CWNGJS7',
+  apiKey: 'aaaaaaa',
+  authDomain: 'aaaaaaa',
+  projectId: 'aaaaaaa',
+  storageBucket: 'aaaaaaa',
+  messagingSenderId: 'aaaaaaa',
+  appId: 'aaaaaaa',
+  measurementId: 'aaaaaaa',
 };
