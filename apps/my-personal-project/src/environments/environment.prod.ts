11f942f026da721ababa2a6e70165f0463d8b0f0
Mario Velasco Alonso <mario1velasco@gmail.com>
2025-01-03 17:46:16 +0100

    Feat: Add environment configuration files for production and local setups

---
+++ apps/my-personal-project/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  apiUrl: 'prod',
+  production: true,
+  firebaseApiKey: '',
+  firebasesMessagingSenderId: '',
+  firebaseAppId: '',
+  firebaseMeasurementId: '',
+};
