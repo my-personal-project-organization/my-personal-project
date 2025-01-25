630f104e911c420a4e2a8265c10992cd105082a5
Mario Velasco Alonso <mario1velasco@gmail.com>
2025-01-25 17:07:28 +0100

    7. feat: update test setup to use setupZoneTestEnv and import node-fetch for compatibility

--- apps/my-personal-project/src/test-setup.ts
+++ apps/my-personal-project/src/test-setup.ts
@@ -1,11 +1,17 @@
-// @ts-expect-error https://thymikee.github.io/jest-preset-angular/docs/getting-started/test-environment
-globalThis.ngJest = {
-  testEnvironmentOptions: {
-    errorOnUnknownElements: true,
-    errorOnUnknownProperties: true,
-  },
-};
-import 'jest-preset-angular/setup-jest';
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+
+setupZoneTestEnv({
+  errorOnUnknownElements: true,
+  errorOnUnknownProperties: true,
+});
 
 import '@angular/localize/init';
 global.TextEncoder = TextEncoder;
+
+// ? Import fetch to fix --> ReferenceError: fetch is not defined. Inside ddbb.service.spec.ts
+import fetch from 'node-fetch';
+
+(globalThis as any).fetch = fetch;
+(globalThis as any).Response = fetch.Response;
+(globalThis as any).Headers = fetch.Headers;
+(globalThis as any).Request = fetch.Request;
