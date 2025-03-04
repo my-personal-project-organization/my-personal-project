faa94ed90e987b23318cfa873825091c560efe55
Mario Velasco Alonso <mario1velasco@gmail.com>
2025-03-04 11:25:51 +0100

    8. feat(data-access): enhance Firestore entity store with CRUD operations and query support

--- libs/shared/data-access/src/lib/state/firestore-entity-store.ts
+++ libs/shared/data-access/src/lib/state/firestore-entity-store.ts
@@ -1,6 +1,7 @@
 import { inject } from '@angular/core';
+import { QueryConstraint } from '@angular/fire/firestore';
 import { patchState, signalStoreFeature, withMethods } from '@ngrx/signals';
-import { addEntity, setAllEntities } from '@ngrx/signals/entities';
+import { addEntity, removeEntity, setAllEntities, setEntity, updateEntity } from '@ngrx/signals/entities';
 import { ZodSchema } from 'zod';
 import { Entity } from '../models';
 import { FirestoneService } from '../services/firestone.service';
@@ -26,9 +27,9 @@ export function witFirestoreEntityStore<T extends Entity>(collectionName: string
             const newEntity = await firestoneService.getOne<T>(collectionName, docRef.id);
             if (newEntity) {
               const validEntity = schema.parse(newEntity);
-              patchState(store, addEntity(validEntity, { collection: collectionName }));
+              patchState(store, addEntity(validEntity));
             }
-            // patchState(store, { loading: false });
+            patchState(store, { loading: false });
             return newEntity;
           } catch (error: any) {
             console.error(error || 'Failed to add entity');
@@ -44,7 +45,7 @@ export function witFirestoreEntityStore<T extends Entity>(collectionName: string
           try {
             const entities = await firestoneService.getMany<T>(collectionName);
             const validEntities = entities.map((entity) => schema.parse(entity));
-            patchState(store, setAllEntities(validEntities), { collection: collectionName });
+            patchState(store, setAllEntities(validEntities));
             patchState(store, { loading: false });
           } catch (error: any) {
             console.error(error || 'Failed to load data');
@@ -55,73 +56,81 @@ export function witFirestoreEntityStore<T extends Entity>(collectionName: string
           }
         },
 
-        //   async loadByQuery(...queryConstraints: QueryConstraint[]) {
-        //     patchState(store, { loading: true, error: null });
-        //     try {
-        //       const entities = await firestoneService.queryMany<T>(
-        //         collectionName,
-        //         ...queryConstraints,
-        //       );
-        //       const validEntities = entities.map((entity) => schema.parse(entity));
-        //       patchState(store, setAllEntities(validEntities, {collection: collectionName}));
-        //       patchState(store, { loading: false });
-        //     } catch (error: any) {
-        //       patchState(store, {
-        //         loading: false,
-        //         error: error.message || 'Failed to load data',
-        //       });
-        //     }
-        //   },
+        async loadOne(id: string) {
+          patchState(store, { loading: true, error: null });
+          try {
+            const entity = await firestoneService.getOne<T>(collectionName, id);
+            if (entity) {
+              const validEntity = schema.parse(entity);
+              patchState(store, setEntity(validEntity));
+            }
+            patchState(store, { loading: false });
+          } catch (error: any) {
+            console.error(error || 'Failed to load one entity');
+            patchState(store, {
+              loading: false,
+              error: error.message || 'Failed to load one entity',
+            });
+          }
+        },
 
-        //   async loadOne(id: string) {
-        //     patchState(store, { loading: true, error: null });
-        //     try {
-        //       const entity = await firestoneService.getOne<T>(collectionName, id);
-        //       if (entity) {
-        //         const validEntity = schema.parse(entity);
-        //         patchState(store, setEntities([validEntity],{collection: collectionName})); // Use setEntities
-        //       }
-        //       patchState(store, { loading: false });
-        //     } catch (error: any) {
-        //       patchState(store, {
-        //         loading: false,
-        //         error: error.message || 'Failed to load entity',
-        //       });
-        //     }
-        //   },
-        //   async update(id: string, changes: Partial<T>) {
-        //     patchState(store, { loading: true, error: null });
-        //     try {
-        //       // Partial validation, but exclude 'id'
-        //       const validatedChanges = schema.partial({id: false}).parse(changes);
+        async delete(id: string) {
+          patchState(store, { loading: true, error: null });
+          try {
+            await firestoneService.delete(collectionName, id);
+            patchState(store, removeEntity(id));
+            patchState(store, { loading: false });
+          } catch (error: any) {
+            console.error(error || 'Failed to delete entity');
+            patchState(store, {
+              loading: false,
+              error: error.message || 'Failed to delete entity',
+            });
+          }
+        },
 
-        //       await firestoneService.update(collectionName, id, {...validatedChanges, updatedAt: new Date()});
-        //       const updatedEntity = await firestoneService.getOne<T>(collectionName, id);
+        async loadByQuery(...queryConstraints: QueryConstraint[]) {
+          patchState(store, { loading: true, error: null });
+          try {
+            const entities = await firestoneService.queryMany<T>(collectionName, ...queryConstraints);
+            const validEntities = entities.map((entity) => schema.parse(entity));
+            patchState(store, setAllEntities(validEntities));
+            patchState(store, { loading: false });
+          } catch (error: any) {
+            console.error(error || 'Failed to load data by query');
+            patchState(store, {
+              loading: false,
+              error: error.message || 'Failed to load data by query',
+            });
+          }
+        },
 
-        //       if(updatedEntity) {
-        //         const validEntity = schema.parse(updatedEntity)
-        //         patchState(store, setEntities([validEntity], {collection: collectionName}));
-        //       }
-        //       patchState(store, { loading: false });
-        //        return updatedEntity;
-        //     } catch (error: any) {
-        //       patchState(store, { loading: false, error: error.message || 'Failed to update entity' });
-        //       return undefined;
-        //     }
-        //   },
+        async update(id: string, changes: Partial<T>) {
+          patchState(store, { loading: true, error: null });
+          try {
+            const validatedChanges = schema.parse(changes);
 
-        //   async delete(id: string) {
-        //     patchState(store, { loading: true, error: null });
-        //     try {
-        //       await firestoneService.delete(collectionName, id);
-        //       patchState(store, { loading: false });
-        //     } catch (error: any) {
-        //       patchState(store, {
-        //         loading: false,
-        //         error: error.message || 'Failed to delete entity',
-        //       });
-        //     }
-        //   },
+            await firestoneService.update(collectionName, id, { ...validatedChanges, updatedAt: new Date() });
+            const updatedEntity = await firestoneService.getOne<T>(collectionName, id);
+
+            if (updatedEntity) {
+              const validEntity = schema.parse(updatedEntity);
+              patchState(
+                store,
+                updateEntity({
+                  id: validEntity.id,
+                  changes: validEntity,
+                }),
+              );
+            }
+            patchState(store, { loading: false });
+            return updatedEntity;
+          } catch (error: any) {
+            console.error(error || 'Failed to update entity');
+            patchState(store, { loading: false, error: error.message || 'Failed to update entity' });
+            return undefined;
+          }
+        },
       };
     }),
   );
