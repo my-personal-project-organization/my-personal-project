0873df1350adcbef051214ca4332931b3f98d24b
Mario Velasco Alonso <mario1velasco@gmail.com>
2025-03-07 09:15:32 +0100

    24. perf(config): Angular zoneless, add bundle analysis script and fix localize warning

--- apps/my-personal-project/src/app/app.config.ts
+++ apps/my-personal-project/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
@@ -8,7 +8,10 @@ import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    // ? https://dev.to/danywalls/angular-19-and-zoneless-1of9 https://angularengineering.com/2024/05/27/angular-18-how-to-remove-zone-js/
+    provideExperimentalZonelessChangeDetection(),
+    // ? https://www.linkedin.com/posts/koustubhmishra_angular-angular-zone-activity-7249282431067840512-YJoY
+    // provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
