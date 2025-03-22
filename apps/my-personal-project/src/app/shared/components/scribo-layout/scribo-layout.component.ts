294bd363ab538b280a6d307ab0314f5658faabad
Mario Velasco Alonso <mario1velasco@gmail.com>
2025-03-22 08:29:26 +0100

    45. refactor(scribo-layout): remove ScriboLayout component and associated files

--- apps/my-personal-project/src/app/shared/components/scribo-layout/scribo-layout.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-scribo-layout',
-  templateUrl: './scribo-layout.component.html',
-  styleUrls: ['./scribo-layout.component.scss'],
-})
-export class ScriboLayoutComponent {}
