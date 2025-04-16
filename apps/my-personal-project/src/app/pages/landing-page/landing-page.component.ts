410415c1de5bd43e18e306aec67df3ecbb8cda02
Mario Velasco Alonso <mario1velasco@gmail.com>
2025-04-16 16:18:28 +0200

    chore(51-landing-page): remove unused code and testing functions
    - Cleaned up the LandingPageComponent by removing commented-out code
      and testing functions that are no longer needed.
    - This helps improve code readability and maintainability.

--- apps/my-personal-project/src/app/pages/landing-page/landing-page.component.ts
+++ apps/my-personal-project/src/app/pages/landing-page/landing-page.component.ts
@@ -1,14 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { NewArticleSchema } from '@mpp/scribo/data-access';
 import { DialogComponent } from '@mpp/shared/ui';
 import { NgForTranslateDirective } from '../../shared/directives/ng-for-translate.directive';
 import { TranslatePipe } from '../../shared/pipes/translate.pipe';
 import { PlacesService } from '../../shared/services/places.service';
 import { TranslationService } from '../../shared/services/translations/translation.service';
-import { Place } from '../../shared/types/place';
-import { LandingPageService } from './landing-page.service';
 import { FooterComponent } from './components/footer/footer.component';
+import { LandingPageService } from './landing-page.service';
 
 @Component({
   selector: 'app-landing-page',
@@ -32,74 +30,13 @@ export class LandingPageComponent {
     this.dialogTitle = this.translationService.translate(experience);
     this.listKey = listKey;
     this.dialogOpen = true;
-    this.saveData();
   }
 
   closeDialog() {
     this.dialogOpen = false;
-    this.listPlaces();
   }
 
   onCLickScriboProject() {
     this.router.navigate(['/scribo']);
-  }
-
-  // TODO: REMOVE just testing
-  async listPlaces() {
-    const places = await this.placesService.getPlaces();
-    places.subscribe((data) => {
-      console.log('Places:', data);
-    });
-  }
-
-  // TODO: REMOVE just testing
-  async saveData() {
-    const place: Place = {
-      name: 'My Place 2',
-      location: {
-        lat: 10,
-        lng: 10,
-      },
-      description: 'My Description',
-      imageUrl: 'My Image Url',
-    };
-    const response = await this.placesService.addPlace(place);
-    console.log('Response:', response);
-  }
-
-  // TODO: REMOVE just testing
-  testZod() {
-    const articleData = {
-      _id: 'some-id',
-      userId: 'user-id',
-      mainTitle: 'My Great Article',
-      content: [
-        {
-          title: 'Section 1',
-          items: [
-            { type: 'description', value: '<p>Some formatted HTML</p>' },
-            {
-              type: 'image',
-              value: 'https://example.com/image.jpg',
-              footer: 'My Image',
-            },
-          ],
-        },
-      ],
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
-
-    const validatedArticle = NewArticleSchema.parse(articleData); // Throws ZodError if validation fails
-
-    // Or use safeParse:
-    const result = NewArticleSchema.safeParse(articleData);
-    if (result.success) {
-      const validatedArticle = result.data;
-      // Do something with the validated article
-    } else {
-      // Handle the error (result.error)
-      console.error(result.error.format());
-    }
   }
 }
