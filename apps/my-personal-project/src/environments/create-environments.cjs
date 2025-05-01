7ee4aba8a1dd5a40a96a458957b64b9df5720eeb
Mario Velasco Alonso <mario1velasco@gmail.com>
2025-05-01 07:47:14 +0200

    feat(53-fix-ci): add environment creation script and update tsconfig
    - Introduced a new target for creating environment files in project.json.
    - Updated the create-environments script to generate environment.ts and environment.prod.ts.
    - Enhanced tsconfig.app.json to include "node" types for better type support.

--- apps/my-personal-project/src/environments/create-environments.cjs
+++ apps/my-personal-project/src/environments/create-environments.cjs
@@ -1,44 +1,54 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
-const successColor = '\x1b[32m%s\x1b[0m';
-const checkSign = '\u{2705}';
-const envList = ['local', 'prod'];
+const dotenv = require('dotenv');
 
-// ? Follow: https://medium.com/@ragabon/create-multiple-environment-ts-in-angular-using-env-and-automate-using-node-js-933503fca752
+// Load environment variables from .env file in the app root
+const envPath = path.resolve(__dirname, '../../.env');
+const envConfig = dotenv.config({ path: envPath }).parsed || {};
 
-function createEnvironments() {
-  envList.forEach((en) => {
-    const envPath = en !== 'local' ? `.${en}` : '';
-    const apiUrl = en === 'local' ? 'local' : 'prod';
-    const production = en === 'local' ? false : true;
+const targetPath = path.resolve(__dirname, './environment.ts');
+const targetProdPath = path.resolve(__dirname, './environment.prod.ts');
 
-    const envFile = `export const environment = {
-      apiUrl: '${apiUrl}',
-      production: ${production},
-      firebaseConfig: {
-        apiKey: '${process.env.FIREBASE_API_KEY}',
-        authDomain: 'my-personal-project-7c70d.firebaseapp.com',
-        projectId: 'my-personal-project-7c70d',
-        storageBucket: 'my-personal-project-7c70d.firebasestorage.app',
-        messagingSenderId: '938956948971',
-        appId: '1:938956948971:web:34ade1fc4fb53a773a45ae',
-        measurementId: 'G-422CWNGJS7',
-      }
-    };`;
+const envFileContent = `
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* eslint-disable */
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: '${envConfig.FIREBASE_API_KEY || ''}',
+    authDomain: 'my-personal-project-7c70d.firebaseapp.com',
+    projectId: 'my-personal-project-7c70d',
+    storageBucket: 'my-personal-project-7c70d.firebasestorage.app',
+    messagingSenderId: '938956948971',
+    appId: '1:938956948971:web:34ade1fc4fb53a773a45ae',
+    measurementId: 'G-422CWNGJS7',
+  },
+  // Add other environment variables here
+};
+/* eslint-enable */
+`;
 
-    const targetPath = path.join(__dirname, `../environments/environment${envPath}.ts`);
-    fs.writeFile(targetPath, envFile, (err) => {
-      if (err) {
-        console.error(err);
-        throw err;
-      } else {
-        console.log(successColor, `${checkSign} Successfully generated ${en} environment file`);
-      }
-    });
-  });
-}
+const envProdFileContent = `
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* eslint-disable */
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: '${envConfig.FIREBASE_API_KEY || ''}',
+    authDomain: 'my-personal-project-7c70d.firebaseapp.com',
+    projectId: 'my-personal-project-7c70d',
+    storageBucket: 'my-personal-project-7c70d.firebasestorage.app',
+    messagingSenderId: '938956948971',
+    appId: '1:938956948971:web:34ade1fc4fb53a773a45ae',
+    measurementId: 'G-422CWNGJS7',
+  },
+  // Add other environment variables here
+};
+/* eslint-enable */
+`;
 
-createEnvironments();
-module.exports = { createEnvironments };
+// Write the content to the respective file
+fs.writeFileSync(targetPath, envFileContent);
+console.log(`Output generated at ${targetPath}`);
+fs.writeFileSync(targetProdPath, envProdFileContent);
+console.log(`Output generated at ${targetProdPath}`);
