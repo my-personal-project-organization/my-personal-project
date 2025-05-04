677f37dd2ac4c60067d219c6300f6bb1653aabff
Mario Velasco Alonso <mario1velasco@gmail.com>
2025-05-04 07:49:56 +0200

    49. feat(toast-container): add toast animation for enter and leave transitions
    - Implemented animations for toast component to enhance user experience.
    - Added animation triggers for smooth appearance and disappearance of toasts.

--- libs/shared/ui/src/lib/components/atoms/toast/toast-container/toast-container.component.ts
+++ libs/shared/ui/src/lib/components/atoms/toast/toast-container/toast-container.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ToastComponent } from '../toast.component';
 import { ToastService } from '../toast.service';
+import { trigger, transition, style, animate } from '@angular/animations'; // Import animation functions
 
 @Component({
   selector: 'ui-toast-container',
@@ -9,6 +10,23 @@ import { ToastService } from '../toast.service';
   templateUrl: './toast-container.component.html',
   styleUrls: ['./toast-container.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('toastAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(100%)' }), // Start invisible and below
+        animate(
+          '300ms ease-out',
+          style({ opacity: 1, transform: 'translateY(0)' }), // Fade in and slide up
+        ),
+      ]),
+      transition(':leave', [
+        animate(
+          '300ms ease-in',
+          style({ opacity: 0, transform: 'translateY(50%)' }), // Fade out and slide down slightly
+        ),
+      ]),
+    ]),
+  ],
 })
 export class ToastContainerComponent {
   protected readonly toastService = inject(ToastService);
