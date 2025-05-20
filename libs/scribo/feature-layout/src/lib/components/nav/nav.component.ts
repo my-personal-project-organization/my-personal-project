d8b564b018751eb7ef7d03cf6e68d4c023b9a96b
Mario Velasco Alonso <mario1velasco@gmail.com>
2025-05-20 17:04:53 +0200

    feat(nav-layout): refactor navigation structure and add new nav component
    - remove inline navigation from layout component
    - create separate nav component for better structure and maintainability
    - integrate authentication checks in the new nav component

---
+++ libs/scribo/feature-layout/src/lib/components/nav/nav.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject, TemplateRef, ViewChild } from '@angular/core';
+import { AuthStore } from '@mpp/shared/data-access';
+import { ButtonComponent, NavBarComponent } from '@mpp/shared/ui';
+
+@Component({
+  selector: 'scrb-layout-nav',
+  imports: [NavBarComponent, ButtonComponent],
+  templateUrl: './nav.component.html',
+})
+export class NavComponent {
+  @ViewChild('sidebarTemplate') sidebarTemplate!: TemplateRef<any>;
+  private readonly authStore = inject(AuthStore);
+  isAuthenticated = this.authStore.isAuthenticated;
+  user = this.authStore.user;
+
+  login(): void {
+    this.authStore.signInWithGithub();
+  }
+
+  logout(): void {
+    this.authStore.signOutUser();
+  }
+}
